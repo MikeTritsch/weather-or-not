1882b597e20194290151c736315f7efb97589418
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-08 14:54:42 -0500

    console logged search results

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
-function getApi() {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=4d4a164cfde95971ff68068cb1a1c7b9'
-}
+var searchBtn = $('.citySearchBtn');
+
+searchBtn.on('click', function(event) {
+    event.preventDefault()
+    var location = $('.citySearch').val();
+    console.log(location);
+});
+
+
+// function getApi() {
+//     var location = $('.citySearch').val();
+//     console.log(location);
+// }

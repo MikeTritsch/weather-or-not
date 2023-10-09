e70f771c56f7836bbc9f57393f0643f7125db546
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-08 21:39:00 -0500

    still working on localstorage callback

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
 var searchBtn = $('.citySearchBtn');
+var cityHistory = [];
 
 searchBtn.on('click', function(event) {
     event.preventDefault()
     var location = $('.citySearch').val();
-    var cityLi = $("<li></li>").text(location);
-    $('cityList').append(cityLi);
+    var cityDiv = $('.cityDiv');
+    var newButton = $('<button></button>').text(location);
+    cityDiv.children().eq(2).append(newButton);
+    localStorage.setItem("City Choice", JSON.stringify(location));
     console.log(location);
+    // renderCity()
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// function renderCity() {
+//     var cityStored = JSON.parse(localStorage.getItem("City Choice"));
+//     var cityDiv = $('.cityDiv');
+//     var newButton = $('<button></button>').text(cityStored);
+//     cityDiv.children().eq(2).append(newButton);
+// };
 
 
 // function getApi() {

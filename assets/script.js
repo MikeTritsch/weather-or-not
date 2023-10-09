171208ebe9167960a1cbf74567233e6df0868176
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-09 16:55:34 -0500

    added local storage functions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,62 @@
 var searchBtn = $('.citySearchBtn');
+var citySearch = $('.citySearch');
+var cityList = $('.cityList');
 var cityHistory = [];
 
-searchBtn.on('click', function(event) {
-    event.preventDefault()
-    var location = $('.citySearch').val();
-    var cityDiv = $('.cityDiv');
-    var newButton = $('<button></button>').text(location);
-    cityDiv.children().eq(2).append(newButton);
-    localStorage.setItem("City Choice", JSON.stringify(location));
-    console.log(location);
-    // renderCity()
+
+function renderCity() {
+    $('.cityList').text("");
+
+    for (var i = 0; i < cityHistory.length; i++) {
+        var city = cityHistory[i];
+
+        var li = $("<li></li>");
+        li.text(city);
+        li.attr("data-index", i);
+
+        $('.cityList').append(li);
+    }
+}
+
+function initCity() {
+    var storedCity = JSON.parse(localStorage.getItem("City Choice"));
+
+    if (storedCity !== null) {
+        cityHistory = storedCity;
+    }
+
+    renderCity();
+}
+
+function storeCity() {
+    localStorage.setItem("City Choice", JSON.stringify(cityHistory));
+}
+
+
+$('.cityForm').on("submit", function(event) {
+    event.preventDefault();
+
+    var cityText = $('.citySearch').val();
+
+    if (cityText === ""){
+        return;
+    }
+
+    cityHistory.push(cityText);
+
+    storeCity();
+    renderCity();
 });
 
 
 
+// searchBtn.on('click', function(event) {
+//     event.preventDefault()
+//     var location = $('.citySearch').val();
+//     console.log(location);
 
-
-
-
+//     storeCity();
+//     renderCity();
 
 
 

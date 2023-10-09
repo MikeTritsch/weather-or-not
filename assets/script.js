705e716fc8b6adc9066d039445cde41db3ee525b
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-08 19:39:05 -0500

    more js functionality

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var searchBtn = $('.citySearchBtn');
 searchBtn.on('click', function(event) {
     event.preventDefault()
     var location = $('.citySearch').val();
+    var cityLi = $("<li></li>").text(location);
+    $('cityList').append(cityLi);
     console.log(location);
 });
 
